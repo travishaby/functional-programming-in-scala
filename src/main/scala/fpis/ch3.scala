51149a0acd1e6a58d49537a7856dc40377c7d33c
Travis Haby <travishaby@gmail.com>
2019-06-09 11:44:53 -0600

    Implement exercises 3.2 and 3.3

--- src/main/scala/fpis/ch3.scala
+++ src/main/scala/fpis/ch3.scala
@@ -12,4 +12,19 @@ object LinkedList {
     case Nil => 0
     case Cons(x, xs) => x + sum(xs)
   }
+
+  // Exercise 3.2
+  def tail[A](list: LinkedList[A]): LinkedList[A] = list match {
+    case Nil => Nil
+    case Cons(_, list) => list
+  }
+
+  // Exercise 3.3
+  def setHead[A](list: LinkedList[A], newValue: A): LinkedList[A] = list match {
+    case Nil => sys.error("tail of empty list")
+    case Cons(_, rest) => Cons(newValue, rest)
+  }
+
+  // Exercise 3.4
+  def drop[A](list: LinkedList[A], n: Int): LinkedList[A] = ???
 }
